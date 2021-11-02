eda3e81fca2f4ae1597ebc31afbd95aade2fde4d
lvyq800 <lvyq800@gmail.com>
2021-11-02 13:47:21 +1300

    changed some where

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Firebase CI/CD test for CAITO.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
